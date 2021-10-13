ad30b4cb93cddfdbc4eeef221c02dc6a5e2625e4
Xxxxx Xxxxx <9137644477@mail.ru>
2021-10-13 19:17:38 +0700

    extend templates from base.html

--- products/views.py
+++ products/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop - Главная',
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'GeekShop - Каталог',
+    }
+    return render(request, 'products/products.html', context)
