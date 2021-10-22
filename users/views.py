6b261870faf75fd651582833a54b2cc076bf881a
Xxxxx Xxxxx <9137644477@mail.ru>
2021-10-22 20:47:01 +0700

    added logic for login controller

--- users/views.py
+++ users/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+
 from users.forms import UserLoginForm
 
 
 # Create your views here.
 
 def login(request):
-    form = UserLoginForm()
-    context = {
-        'title': 'GeekShop - Авторизация',
-        'form': form,
-    }
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+    context = {'title': 'GeekShop - Авторизация', 'form': form}
     return render(request, 'users/login.html', context)
 
 
