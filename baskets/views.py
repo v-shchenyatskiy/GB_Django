3202872e1dcb264cf494906fefcaff8ae9a091c2
Xxxxx Xxxxx <9137644477@mail.ru>
2021-10-26 13:26:17 +0700

    added basket_remove() controller

--- baskets/views.py
+++ baskets/views.py
@@ -18,3 +18,9 @@ def basket_add(request, product_id):
         basket.save()
 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, id):
+    basket = Basket.objects.get(id=id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
