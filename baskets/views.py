e3e710574bb9315aa8b865e62da53c5a853b2a7a
Xxxxx Xxxxx <9137644477@mail.ru>
2021-10-26 13:03:31 +0700

    created Basket model and basket_add() controller

--- baskets/views.py
+++ baskets/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+
+from products.models import Product
+from baskets.models import Basket
+
 
 # Create your views here.
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
