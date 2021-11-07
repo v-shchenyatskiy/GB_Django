b40a72aab5988a3242065cf9445637ccb910bf44
Xxxxx Xxxxx <9137644477@mail.ru>
2021-11-07 18:14:19 +0700

    refactor baskets methods

--- baskets/models.py
+++ baskets/models.py
@@ -15,13 +15,15 @@ class Basket(models.Model):
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт {self.product.name}'
 
+    @property
+    def baskets(self):
+        return Basket.objects.filter(user=self.user)
+
     def sum(self):
         return self.product.price * self.quantity
 
     def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user)
-        return sum(basket.sum() for basket in baskets)
+        return sum(basket.sum() for basket in self.baskets)
 
     def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
-        return sum(basket.quantity for basket in baskets)
+        return sum(basket.quantity for basket in self.baskets)
