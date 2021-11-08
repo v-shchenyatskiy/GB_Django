00d2690f58a81eb49965c56893fea3ddaf8c80b0
Xxxxx Xxxxx <9137644477@mail.ru>
2021-11-08 20:39:31 +0700

    created CREATE for admin users

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 
 # Create your views here.
@@ -13,7 +15,14 @@ def index(request):
 # CRUD
 # Create
 def admin_users_create(request):
-    context = {'title': 'GeekShop - Создание пользователей'}
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'GeekShop - Создание пользователей', 'form': form}
     return render(request, 'admins/admin-users-create.html', context)
 
 
