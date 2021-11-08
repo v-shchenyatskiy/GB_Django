aca54f42110d46f65ce06e7ed49a9480660c2352
Xxxxx Xxxxx <9137644477@mail.ru>
2021-11-08 20:06:41 +0700

    created READ for admin users

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from users.models import User
+
 
 # Create your views here.
 
@@ -17,7 +19,10 @@ def admin_users_create(request):
 
 # Read
 def admin_users(request):
-    context = {'title': 'GeekShop - Пользователи'}
+    context = {
+        'title': 'GeekShop - Пользователи',
+        'users': User.objects.all(),
+    }
     return render(request, 'admins/admin-users-read.html', context)
 
 
