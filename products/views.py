491d704045d9ee1a6e761df13e16e07b28dcee92
Xxxxx Xxxxx <9137644477@mail.ru>
2021-11-09 00:17:41 +0700

    added filtering by category

--- products/views.py
+++ products/views.py
@@ -6,16 +6,17 @@ from products.models import ProductCategory, Product
 # Create your views here.
 
 def index(request):
-    context = {
-        'title': 'GeekShop - Главная',
-    }
+    context = {'title': 'GeekShop - Главная'}
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    context = {
-        'title': 'GeekShop - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': Product.objects.all(),
-    }
+def products(request, category_id=None):
+    context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        # category = ProductCategory.objects.get(id=category_id)
+        # products = Product.objects.filter(category=category)
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    context['products'] = products
     return render(request, 'products/products.html', context)
