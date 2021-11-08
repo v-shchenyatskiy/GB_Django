f116fe1b5cdecc833a9dfa6420d80b24942afdce
Xxxxx Xxxxx <9137644477@mail.ru>
2021-11-08 19:20:02 +0700

    created admins app

---
+++ admins/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    context = {'title': 'GeekShop - Админ-панель'}
+    return render(request, 'admins/index.html', context)
+
+
+# CRUD
+# Create
+def admin_users_create(request):
+    context = {'title': 'GeekShop - Создание пользователей'}
+    return render(request, 'admins/admin-users-create.html', context)
+
+
+# Read
+def admin_users(request):
+    context = {'title': 'GeekShop - Пользователи'}
+    return render(request, 'admins/admin-users-read.html', context)
+
+
+# Update
+def admin_users_update(request):
+    context = {'title': 'GeekShop - Обновление пользователя'}
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+# Delete
+def admin_users_delete(request):
+    pass
