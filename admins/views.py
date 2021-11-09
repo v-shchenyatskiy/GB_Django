7ef6b43b747b60bb616da13edd722f3a03ef153b
Xxxxx Xxxxx <9137644477@mail.ru>
2021-11-09 20:55:10 +0700

    added ListView

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
@@ -15,6 +16,8 @@ def index(request):
 
 # CRUD
 # Create
+
+
 @user_passes_test(lambda u: u.is_staff)
 def admin_users_create(request):
     if request.method == 'POST':
@@ -29,13 +32,15 @@ def admin_users_create(request):
 
 
 # Read
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    context = {
-        'title': 'GeekShop - Пользователи',
-        'users': User.objects.all(),
-    }
-    return render(request, 'admins/admin-users-read.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+
+
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users(request):
+#     context = {'title': 'GeekShop - Пользователи', 'users': User.objects.all()}
+#     return render(request, 'admins/admin-users-read.html', context)
 
 
 # Update
