a8802353d362f70c01eed5ab029548f43241e045
Xxxxx Xxxxx <9137644477@mail.ru>
2021-11-09 20:47:40 +0700

    added Pagination

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from products.models import ProductCategory, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -10,7 +11,7 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
     if category_id:
         # category = ProductCategory.objects.get(id=category_id)
@@ -18,5 +19,14 @@ def products(request, category_id=None):
         products = Product.objects.filter(category_id=category_id)
     else:
         products = Product.objects.all()
-    context['products'] = products
+
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    context['products'] = products_paginator
     return render(request, 'products/products.html', context)
